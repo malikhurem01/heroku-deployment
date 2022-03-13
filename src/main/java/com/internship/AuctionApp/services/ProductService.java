606ecac6e955923b81081e7bec097cb628396f3e
malikhurem01 <40504567+malikhurem01@users.noreply.github.com>
2022-03-13 16:30:52 +0100

    Delete src directory

--- src/main/java/com/internship/AuctionApp/services/ProductService.java
+++
@@ -1,9 +0,0 @@
-package com.internship.AuctionApp.services;
-
-import com.internship.AuctionApp.Models.Product;
-
-public interface ProductService {
-    public Product getProduct(Long id);
-
-    public Product saveProduct(Product product);
-}
