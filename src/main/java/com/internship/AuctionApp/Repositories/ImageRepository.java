49961129eeca31af259a189a179bed2c5ccb0510
malikhurem01 <40504567+malikhurem01@users.noreply.github.com>
2022-03-13 20:46:24 +0100

    Add files via upload

---
+++ src/main/java/com/internship/AuctionApp/Repositories/ImageRepository.java
@@ -0,0 +1,16 @@
+package com.internship.AuctionApp.Repositories;
+
+import com.internship.AuctionApp.Models.Image;
+import com.internship.AuctionApp.Models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+    @Query("select b from Image b where b.product_id = :product_id")
+    List<Image> findAllByProduct_id(@Param("product_id") Product product_id);
+
+}
